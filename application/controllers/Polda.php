ebba19b2d1c81a595588e064a4a4cbb6a9972ff7
Muhammad Rifqi <muhammad45rifki@gmail.com>
2026-07-11 21:11:40 +0700

    update

---
+++ application/controllers/Polda.php
@@ -0,0 +1,18 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Polda extends CI_Controller {
+
+    public function __construct() {
+        parent::__construct();
+        $this->load->helper('url');
+        $this->load->library('session');
+        $this->load->helper('uuid');
+        $this->load->helper('string');
+    }
+
+    public function index()
+    {
+       
+    }
+}
