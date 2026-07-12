89e494007057b8a63756b5685df3232351324872
Muhammad Rifqi <muhammad45rifki@gmail.com>
2026-07-12 08:00:21 +0700

    update

--- application/controllers/Role.php
+++ application/controllers/Role.php
@@ -19,21 +19,31 @@ class Role extends CI_Controller {
      public function get()
     {
 
-        //$headers = $this->input->request_headers();
-        //$content_type = $headers['Authorization'] ?? null;
+        $headers = $this->input->request_headers();
+        $authorization = $headers['Authorization'];
+        if($authorization != null){
+            $data = $this->db->query("select * from tbl_role")->result_array();
+            echo json_encode(array("message"=> "success", "status" => 200 , "data" => $data));
+        }else{
+            echo json_encode("Unauthorize");
+        }
 
-        $data = $this->db->query("select * from tbl_role")->result_array();
-        echo json_encode(array("message"=> "success", "status" => 200 , "data" => $data));
     }
 
     public function post()
     {
-        $rows = json_decode($this->input->raw_input_stream, true);
-        $data = $this->db->query("insert into tbl_role(roles,created_at)values('".$rows['role']."','".date('Y-m-d : H:i:s')."')");
-        if($data){
-            echo json_encode(array("message"=> "success", "status" => 200 , "data" => $rows));
+        $headers = $this->input->request_headers();
+        $authorization = $headers['Authorization'];
+        if($authorization != null){
+            $rows = json_decode($this->input->raw_input_stream, true);
+            $data = $this->db->query("insert into tbl_role(roles,created_at)values('".$rows['role']."','".date('Y-m-d : H:i:s')."')");
+            if($data){
+                echo json_encode(array("message"=> "success", "status" => 200 , "data" => $rows));
+            }else{
+                echo json_encode(array("message"=> "failed", "status" => 400 , "data" => []));
+            }
         }else{
-            echo json_encode(array("message"=> "failed", "status" => 400 , "data" => []));
+            echo json_encode("Unauthorize");
         }
     }
 
