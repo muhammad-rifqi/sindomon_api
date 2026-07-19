ff005528019268c8e6e6a8e954603633efe30e61
Muhammad Rifqi <muhammad45rifki@gmail.com>
2026-07-19 15:28:51 +0700

    udpate

--- application/controllers/Polda.php
+++ application/controllers/Polda.php
@@ -5,14 +5,50 @@ class Polda extends CI_Controller {
 
     public function __construct() {
         parent::__construct();
+
+        header("Access-Control-Allow-Origin: *");
+        header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+        header("Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Requested-With");
+        header("Access-Control-Allow-Credentials: false");
+
+        if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+            http_response_code(200);
+            exit();
+        }
+
+        $this->config->load('jwt');
+        $this->load->helper('jwt');
         $this->load->helper('url');
         $this->load->library('session');
         $this->load->helper('uuid');
         $this->load->helper('string');
     }
 
-    public function index()
+    public function get()
     {
-       // pake $this->db->query("") aja. soalnya ini bakalan banyak query complex. 
+        $headers = $this->input->request_headers();
+        if(isset($headers['Authorization'])){
+            $authorization = $headers['Authorization'];
+            $payload = jwt_decode($authorization);
+             if ($payload === false) {
+                echo json_encode("Unauthorize");
+             } else {
+                $data = $this->db->query("select * from tbl_polda")->result_array();
+                $rows = array();
+                for($i=0;$i<count($data);$i++){
+                    $rows[] = array(
+                        "id" => $data[0]['id'],
+                        "nama_polda" => $data[0]['nama_polda'],
+                        "latitude" => $data[0]['latitude'],
+                        "longitude" => $data[0]['longitude'],
+                        "created_at" => $data[0]['created_at'],
+                        "polres" => $this->db->query("select * from tbl_polres where polda_id = '".$data[0]['id']."'")->result_array(),
+                    );
+                }
+                echo json_encode(array("message"=> "success", "status" => 200 , "data" => $rows));
+             }
+        }else{
+            echo json_encode("Unauthorize");
+        }   
     }
 }
